241387da4c98e502ab8fcfd3622b2309337aed54
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-12-13 08:29:58 +0530

    Bug fix

--- todoApp/config/db.js
+++ todoApp/config/db.js
@@ -1,11 +1,25 @@
+// const mongoose = require("mongoose");
+// const connectDb = () => {
+//   mongoose
+//     .connect(
+//       "mongodb+srv://jatin4224:MyrigWDrXvTCGMeB@cluster4224.cctoa.mongodb.net/todoapp"
+//     )
+//     .then("connection success")
+//     .catch((err) => console.log(err));
+// };
+
+// module.exports = {
+//   connectDb,
+// };
 const mongoose = require("mongoose");
+
 const connectDb = () => {
   mongoose
     .connect(
       "mongodb+srv://jatin4224:MyrigWDrXvTCGMeB@cluster4224.cctoa.mongodb.net/todoapp"
     )
-    .then("connection success")
-    .catch((err) => console.log(err));
+    .then(() => console.log("Database connection successful"))
+    .catch((err) => console.error("Database connection error:", err));
 };
 
 module.exports = {
