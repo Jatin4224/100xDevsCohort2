135a05f553b0fcc10904f5dbe29da3eb710cc28c
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-03-07 23:08:16 +0530

    prismaclient

--- week-18-Prisma/src/index.ts
+++ week-18-Prisma/src/index.ts
@@ -1,1 +1,15 @@
-console.log("hey there");
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+async function createUser() {
+  await client.user.create({
+    data: {
+      username: "Jai",
+      password: "jatinsh",
+      age: 21,
+    },
+  });
+}
+
+createUser();
