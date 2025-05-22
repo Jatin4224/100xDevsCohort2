71ee130f713dfc83db50bf1038b7030394f7a26c
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-05-22 22:17:46 +0530

    .

--- React/basic/src/App.jsx
+++ React/basic/src/App.jsx
@@ -3,6 +3,36 @@ import Navbar from "./components/Navbar";
 import "./index.css";
 
 function App() {
+  const data = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1746961072878-782f9bfe99f8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bXVzaWMlMjBhcnRpc3QlMjBwb3J0cmFpdHxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Ghost",
+      artist: "justin",
+      added: false,
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1693835777292-cf103dcd2324?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bXVzaWMlMjBhcnRpc3QlMjBwb3J0cmFpdHxlbnwwfHwwfHx8MA%3D%3D",
+      name: "I lived",
+      artis: "one republic",
+      added: false,
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1746961072878-782f9bfe99f8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bXVzaWMlMjBhcnRpc3QlMjBwb3J0cmFpdHxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Ghost",
+      artist: "justin",
+      added: false,
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1693835777292-cf103dcd2324?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bXVzaWMlMjBhcnRpc3QlMjBwb3J0cmFpdHxlbnwwfHwwfHx8MA%3D%3D",
+      name: "I lived",
+      artis: "one republic",
+      added: false,
+    },
+  ];
   return (
     <>
       <div
