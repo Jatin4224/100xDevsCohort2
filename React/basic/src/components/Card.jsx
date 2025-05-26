ad4d268d5d2ddc79a9b55246c7732b9f2266f482
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-05-26 10:21:09 +0530

    form project done

--- React/basic/src/components/Card.jsx
+++ React/basic/src/components/Card.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ user, handleRemove, id }) => {
   return (
-    <div className="w-42 h-[30vh] bg-zinc-100 rounded-lg padding flex flex-col items-center gap-2">
-      <div className=" w-[4vw] h-[3vw] rounded-sm bg-zinc-200 overflow-hidden">
-        <img className="w-full h-full object-cover" src="" alt="" />
+    <div className="w-52 h-[40vh] bg-zinc-100 rounded-lg padding flex flex-col items-center gap-2 shadow-md">
+      <div className="w-[4vw] h-[3vw] rounded-sm bg-zinc-200 overflow-hidden">
+        <img
+          className="w-full h-full object-cover"
+          src={user.image}
+          alt={user.name}
+        />
       </div>
-      <h1 className="mt-1 text-xl font-semibold">Jatin</h1>
+      <h1 className="mt-1 text-xl font-semibold">{user.name}</h1>
+      <h4 className="opacity-60 text-xs font-semibold ">{user.email}</h4>
       <p className="mt-1 text-center text-xs font-semibold leading-3">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
         error.
       </p>
-      <button className="padding-button bg-red-500 text-xs rounded-sm font-semibold text-white mt-4">
+      <button
+        onClick={() => handleRemove(id)}
+        className="padding-button bg-red-500 text-xs rounded-sm font-semibold text-white mt-4"
+      >
         Remove it
       </button>
     </div>
--- React/basic/src/components/Form.jsx
+++ React/basic/src/components/Form.jsx
@@ -1,7 +1,43 @@
-import React from "react";
-
-const Form = () => {
-  return <div>Form</div>;
+import { useForm } from "react-hook-form";
+const Form = ({ handleFormSubmitData }) => {
+  const { register, handleSubmit, reset } = useForm();
+  const handleFormSubmit = (data) => {
+    handleFormSubmitData(data);
+    reset();
+  };
+  (data) => handleFormSubmitData(data);
+  return (
+    <div className="mt-12 flex gap-12 justify-center padding">
+      <form
+        className="flex gap-12 bg-zinc-100  shadow-lg rounded-sm"
+        action=""
+        onSubmit={handleSubmit(handleFormSubmit)}
+      >
+        <input
+          {...register("name")}
+          className="rounded-md px-2 py-1 text-base font-semibold outline-none"
+          type="text"
+          placeholder="name"
+        />
+        <input
+          {...register("email")}
+          className="rounded-md px-2 py-1 text-base font-semibold outline-none"
+          type="text"
+          placeholder="email"
+        />
+        <input
+          {...register("image")}
+          className="rounded-md px-2 py-1 text-base font-semibold outline-none"
+          type="text"
+          placeholder="image url"
+        />
+        <input
+          className=" rounded-md padding-button py-1 bg-blue-500 text-white font-semibold hover:bg-blue-100 hover:text-blue-400"
+          type="submit"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Form;
