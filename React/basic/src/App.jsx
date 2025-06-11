a0046c07f1cd00d89ede2d34dfff3eb8590b224c
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-06-11 18:50:12 +0530

    poject02-redux

--- React/basic/src/App.jsx
+++ React/basic/src/App.jsx
@@ -1,37 +1,34 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  decrement,
-  increment,
-  incrementAsync,
-  incrementByAmount,
-} from "./store/reducers/counterSlice";
-
+import { userDelete } from "./store/reducers/UserReducer";
 function App() {
-  const { value } = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
+  const { users } = useSelector((state) => state.UserReducer);
+  const Dispatch = useDispatch();
 
+  const DeleteHandler = (id) => {
+    Dispatch(userDelete(id));
+  };
   return (
     <>
-      <h1 className="text-5xl">Counter: {value}</h1>
-      <div className="flex gap-6">
-        <button
-          className=" border-b-4 hover:text-red-500 "
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <button
-          className="border-b-4 hover:text-red-500"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-        <button
-          className="border-b-4 hover:text-red-500"
-          onClick={() => dispatch(incrementAsync(5))}
-        >
-          incrementByAmount
-        </button>
+      <div className="m-auto container p-8 mt-5 bg-gray-600 ">
+        <h1 className="text-2xl font-bold text-white ">User List</h1>
+        <ul>
+          {users.map((user, index) => {
+            return (
+              <li key={index}>
+                <h1 className="bg-gray-400 text-gray-800">
+                  {user.name} {""}
+                  <span
+                    onClick={() => DeleteHandler(user.id)}
+                    className="text-red-600 font-black cursor-pointer hover:text-red-800"
+                  >
+                    X
+                  </span>
+                </h1>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
